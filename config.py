10cd4ad142f1d5d5dd3cf005a1747e402f1d1926
Wts558 <soboosting.2@gmail.com>
2025-08-06 13:41:26 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7575312604:AAEdwkeosQf4w3Y1mNy-GLEu6seGn5DpNHE"  # токен ботика
+CHANNEL_URL = "https://t.me/+AzNzbztnQJk3Mjdl"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002245634524"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002315290839"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/tinposjag"
+ADMIN_ID = 6751340797 # Ваш айди без скобочек
+PROMO = "Softerai" # промокод
